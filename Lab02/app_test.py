5ee2ed26fab32ab8f025ee11b2739b1d7b25f9b3
Richard <yisin11012@gmail.com>
2023-03-05 14:02:00 +0800

    Update Lab02

--- Lab02/app_test.py
+++ Lab02/app_test.py
@@ -1,7 +1,7 @@
 import unittest
-from unittest.mock import patch, MagicMock, Mock, ANY, call
+from unittest.mock import patch, MagicMock, Mock
 from app import Application,MailSystem
-
+from io import StringIO
 
 class ApplicationTest(unittest.TestCase):
 
@@ -11,12 +11,14 @@ class ApplicationTest(unittest.TestCase):
         self.app = Application()
         self.app.selected = ["William", "Oliver", "Henry"]
         self.app.mailSystem = MailSystem()
-    
+
+        
     @patch('app.Application.get_random_person')
     def test(self, mock_get_random_person):
         #Mock get_random_person(), return values as follows: "William, Oliver, Henry, Liam".
         mock_get_random_person.side_effect = ["William", "Oliver", "Henry","Liam"]
-   
+        self.app.get_random_person = mock_get_random_person       
+        
         #Assure not to select the ones who are already selected
         #Examine the result of select_next_person() using assertEqual.
         person = self.app.select_next_person()
@@ -25,28 +27,28 @@ class ApplicationTest(unittest.TestCase):
 
         self.app.notify_selected()
 
+        #Finish fake_mail()
+        #Spy on send() and write()
+        spy_mailSystem_write = MagicMock(wraps=self.app.mailSystem.write)
+        spy_mailSystem_send = MagicMock(wraps=self.app.mailSystem.send)
         for i in range(len(self.app.selected)):
-            context = self.app.mailSystem.write(self.app.selected[i])
-            self.app.mailSystem.send(self.app.selected[i],context)
-            print(context)
+
+            context = spy_mailSystem_write(self.app.selected[i])
+            spy_mailSystem_send(person,context)
             self.assertEqual(context,f"Congrats, {self.app.selected[i]}!")
 
-        
+            #print the mail context.
 
-        mock = Mock(return_value=None)
-        mock('William')
-        mock('Oliver')
-        mock('Henry')
-        mock('Liam')
-        print(mock.call_args_list)
+            print(context)
 
-        mock = Mock(return_value=None)
-        mock('William', 'Congrats, William!')
-        mock('Oliver', 'Congrats, Oliver!')
-        mock('Henry', 'Congrats, Henry!')
-        mock('Liam', 'Congrats, Liam!')
-        print(mock.call_args_list)
-        
+        #Examine the call count of send() and write() using assertEqual.
+        print("\n\n")
+        self.assertEqual(spy_mailSystem_write.call_count,4)
+        self.assertEqual(spy_mailSystem_send.call_count,4)
+        print(spy_mailSystem_write.call_args_list)
+        print(spy_mailSystem_send.call_args_list)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
