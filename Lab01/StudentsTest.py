6d37b4e6eee5f7feb62d1e22ef446312c10c4986
Richard <yisin11012@gmail.com>
2023-04-13 01:43:14 +0800

    Update Lab01 - add MEX function

--- Lab01/StudentsTest.py
+++ Lab01/StudentsTest.py
@@ -6,6 +6,8 @@ class Test(unittest.TestCase):
 
     user_name = ['John', 'Mary','Thomas','Jane']
     user_id = []
+
+
     
     # test case function to check the Students.set_name function
     def test_0_set_name(self):
@@ -24,8 +26,16 @@ class Test(unittest.TestCase):
 
     # test case function to check the Students.get_name function
     def test_1_get_name(self):
+
+        def mex(array):
+            items = set(array)
+            for i in range(len(array)):
+                if i not in items:
+                    return i
+            return len(array)
+
+    
         print("\nStart get_name test\n")
-        #TODO
 
         #test for all the valid ids
         for i in range(len(self.user_id)):
@@ -33,10 +43,7 @@ class Test(unittest.TestCase):
             self.assertEqual(name, self.user_name[i])
 
         #test for an the invalid ids using Mex
-        self.assertEqual(self.students.get_name(len(self.user_id)), 'There is no such user')
-        #print(self.students.get_name(5))
-        
-
+        self.assertEqual(self.students.get_name(mex(self.user_id)), 'There is no such user')
 
         print(f"\nuser_id length = {len(self.user_id)} \nuser_name length = {len(self.user_name)}\n")
         [print(f"id {id} : {self.students.get_name(id)}") for id in range(5)]
